af697d46daa936ddf102da5c7791b9202a8d9e5c
Emmanuel Garcia <earturoga@gmail.com>
2022-04-21 15:41:26 -0600

    css and about me middle

---
+++ components/sections/contact.js
@@ -0,0 +1,11 @@
+import styles from 
+function Contact() {
+  return (
+    <div>
+      <h3 className={styles.title}>
+        <span className={styles.number}>02.</span> Work
+      </h3>
+    </div>
+  );
+}
+export default Contact;
