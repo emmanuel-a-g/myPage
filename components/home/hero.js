774fc8b3dfe71730f92dddef9a5455efca43a32d
Emmanuel Garcia <earturoga@gmail.com>
2022-04-28 17:04:32 -0600

    fixed resizing

--- components/home/hero.js
+++ components/home/hero.js
@@ -18,9 +18,18 @@ function Hero() {
   }, []);
 
   return (
-    <div className={width < breakpoint ? styles.heroFull : styles.hero} style={highContrastStatus ? {backgroundColor: "#5f5e5e"} : {backgroundColor: "#003b59"}}>
+    <div
+      className={width < breakpoint ? styles.heroFull : styles.hero}
+      style={
+        highContrastStatus
+          ? { backgroundColor: "#5f5e5e" }
+          : { backgroundColor: "#003b59" }
+      }
+    >
       {width < breakpoint ? null : <Aside className={styles.left} />}
-      <Main className={width < breakpoint ? styles.middleFull : styles.middle} />
+      <Main
+        className={width < breakpoint ? styles.middleFull : styles.middle}
+      />
       {width < breakpoint ? null : <AsideRight className={styles.right} />}
     </div>
   );
