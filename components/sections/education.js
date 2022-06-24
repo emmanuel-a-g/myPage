c5b4417ddd91b03255f77702372db7f6bf5d1d04
Emmanuel Garcia <earturoga@gmail.com>
2022-06-23 23:29:51 -0600

    education revisit

--- components/sections/education.js
+++ components/sections/education.js
@@ -6,9 +6,6 @@ function Education() {
       <h3 className={styles.title}>
         <span className={styles.number}>03.</span> Education
       </h3>
-      <div className={styles.iframeBox}>
-        <iframe className={styles.theIframe} src="https://docs.google.com/document/d/e/2PACX-1vQ0gHRDABIHb4v7LB_LRJII3mfFYVUgIwTHyBGrzk7uOKYo1Pw_OsZKlkTIxKDadAOd6VpPLgU1eDLN/pub?embedded=true"></iframe>
-      </div>
     </div>
   );
 }
